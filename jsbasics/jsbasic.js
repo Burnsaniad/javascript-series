2785b2e0c1559431fec5aee3e1cbcda27d88a9cb
Saniad Islam <143685312+Burnsaniad@users.noreply.github.com>
2024-05-06 10:33:35 +0000

    javascript variables done

---
+++ jsbasics/jsbasic.js
@@ -0,0 +1,6 @@
+const accountID = 1234;
+let accountName = "Saniad"
+var accountEmail = "Saniad@gmail.com";
+accountCity = "Gazipur";
+
+console.table([accountID, accountName, accountEmail, accountCity])
