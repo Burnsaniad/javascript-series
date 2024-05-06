6370ea04c05ccf3c6dfde4f2bb8bac8448d3bd61
Saniad Islam <143685312+Burnsaniad@users.noreply.github.com>
2024-05-06 18:24:23 +0000

    datatype done

--- jsbasics/datatype.js
+++ jsbasics/datatype.js
@@ -5,9 +5,16 @@
 // console.log(isScore)
 // console.log(typeof isScore)
 
-let score = "33asd"
-console.log(typeof score)
+// let score = "33asd"
+// console.log(typeof score)
 
-let isScore = Number(score)
-console.log(isScore)
-console.log(typeof isScore)
+// let isScore = Number(score)
+// console.log(isScore)
+// console.log(typeof isScore)
+
+let Bool = "False"
+let booliswhat = Boolean(Bool)
+let number = Number(booliswhat)
+
+console.log(number)
+console.log(typeof number)
