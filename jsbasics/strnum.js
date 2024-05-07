cecafeed550113f2244287343c32d5aba37f27b6
Saniad Islam <143685312+Burnsaniad@users.noreply.github.com>
2024-05-07 04:22:16 +0000

    string number Done

---
+++ jsbasics/strnum.js
@@ -0,0 +1,9 @@
+console.log("1" + 2 + 1)// Cause start with String
+console.log(2 + 2 + "1")// Cause start with Number
+
+console.log(3 + 1 + "2")
+console.log(2 + 1 + "2" + 2 + 1)// Start with Number but at the middle Start with String
+
+let countNumber = 100
+countNumber++;
+console.log(countNumber)
