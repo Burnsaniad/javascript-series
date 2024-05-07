baec6901cb59425d1e631258e0d3db54a8c14c19
Saniad Islam <143685312+Burnsaniad@users.noreply.github.com>
2024-05-07 05:29:09 +0000

    comparision done

---
+++ jsbasics/comparision.js
@@ -0,0 +1,6 @@
+//check only number value
+console.log("3" > 2)
+console.log("03" > 2)
+
+console.log("3" == 3)// check only number and output true
+console.log("3" === 3)//check also datatype and output false
