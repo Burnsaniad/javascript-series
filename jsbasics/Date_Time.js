511d79a0838def1939a5500d927b565492878874
Saniad Islam <143685312+Burnsaniad@users.noreply.github.com>
2024-05-15 11:40:13 +0000

    Date & Time done

---
+++ jsbasics/Date_Time.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString())
+console.log(myDate.toTimeString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toJSON())
+console.log(typeof myDate);
