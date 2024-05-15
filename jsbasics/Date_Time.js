c945497166275f18519b43dff3cdcdc9a98ddc6d
Saniad Islam <143685312+Burnsaniad@users.noreply.github.com>
2024-05-15 16:41:25 +0000

    Date & Time Done

--- jsbasics/Date_Time.js
+++ jsbasics/Date_Time.js
@@ -7,3 +7,12 @@ console.log(myDate.toLocaleDateString())
 console.log(myDate.toLocaleTimeString())
 console.log(myDate.toJSON())
 console.log(typeof myDate);
+
+// Wed May 15 2024 16:40:22 GMT+0000 (Coordinated Universal Time)
+// Wed May 15 2024
+// 16:40:22 GMT+0000 (Coordinated Universal Time)
+// 5/15/2024, 4:40:22 PM
+// 5/15/2024
+// 4:40:22 PM
+// 2024-05-15T16:40:22.903Z
+// object
