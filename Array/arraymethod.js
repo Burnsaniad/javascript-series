997f136f4a77ae9d759f3d06c0e59d196ee0a200
Saniad Islam <143685312+Burnsaniad@users.noreply.github.com>
2024-05-31 17:32:34 +0000

    array method done

--- Array/arraymethod.js
+++ Array/arraymethod.js
@@ -1,3 +1,6 @@
 const myNum = ['a', 'b', 'c', 'd', 'e'];
-myNum.forEach((num) => console.log(num))
+// myNum.forEach((num) => console.log(num))
 
+const num = [1,2,3,4];
+const newNum = num.reduce((acc,curr) => (acc + curr),0)
+console.log(newNum);
