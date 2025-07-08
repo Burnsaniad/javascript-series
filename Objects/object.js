f484737d638a0f43db3f3257a3a46eea30373fd8
Burnsaniad <islamsaniad@gmail.com>
2025-07-08 22:54:17 +0600

    add js object

--- Objects/object.js
+++ Objects/object.js
@@ -1,8 +1,16 @@
-const inventory = {
-    apples: 50,
-    oranges: 30,
-    bananas: 25
-    };
-    for (const [key, value] of Object.entries(inventory)) {
-    console.log(`${key}: ${value}`);
-    }
+// An object representing a person
+const person = {
+  name: "John",
+  age: 30,
+  city: "New York"
+};
+
+// Creating Objects
+// 1. Object Literal Syntax (Most Common)
+// javascript
+const car = {
+  brand: "Toyota",
+  model: "Camry",
+  year: 2022,
+  isElectric: false
+};
