e3939420c7cb07345a469629309937e17090206d
Burnsaniad <islamsaniad@gmail.com>
2025-07-09 23:41:25 +0600

    add some objects methods

--- Objects/object.js
+++ Objects/object.js
@@ -14,3 +14,22 @@ const car = {
   year: 2022,
   isElectric: false
 };
+
+let fruits = {name: "mango"}
+const anotherfruits = {name: "mango"}
+
+
+console.log(fruits == anotherfruits)
+console.log(fruits === anotherfruits)
+
+fruits = anotherfruits
+
+console.log(fruits == anotherfruits)
+console.log(fruits === anotherfruits)
+
+
+obj2 = Object.assign({}, fruits)
+console.log(obj2)
+console.log(fruits)
+
+console.log(obj2 === fruits)
